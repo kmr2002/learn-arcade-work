f0996c59a4b5a47126bb3f3f6096586c96c7fa26
Kyrsten Rehberg <kyrsten.rehberg@my.simpson.edu>
2021-09-02 15:00:58 -0500

    Work on testing

---
+++ Testing/test.py
@@ -0,0 +1,9 @@
+print("Most exciting class ever!!")
+print("Professor Craven said \"hi\"!")
+print("How do I print backslashes? \\")
+print("What does \ndo?")
+print("""Holy cow!
+The professor
+has
+gone
+crazy!""")
