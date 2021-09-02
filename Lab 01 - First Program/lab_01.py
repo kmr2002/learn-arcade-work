fd95a97703bc6e6a322f06c98b030ffe4ee6a921
Kyrsten Rehberg <kyrsten.rehberg@my.simpson.edu>
2021-09-02 15:19:56 -0500

    Work on Lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("We need to make sure to use\nproper English\nfor this lab.")
+print("\\Which seems easy enough\\")
+print("They all need to be different statements.\aAnd there needs to be at least four of them.")
+print("Here is my lab 1.")
