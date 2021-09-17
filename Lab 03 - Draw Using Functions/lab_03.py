cd14364a164476a84b6b20e41cb957047b601dcd
Kyrsten Rehberg <kyrsten.rehberg@my.simpson.edu>
2021-09-17 11:45:02 -0500

    Lab 3 work

--- Lab 03 - Draw Using Functions/lab_03.py
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -33,110 +33,111 @@ def draw_star(x, y):
     arcade.draw_circle_filled(x, y, 7, arcade.csscolor.BEIGE)
 
 def draw_cloud(x, y):
-    arcade.draw_ellipse_filled(x, y, 80, 40, arcade.csscolor.GAINSBORO)
-    arcade.draw_circle_filled(x - 40, y + 5, 15, arcade.csscolor.GAINSBORO)
-    arcade.draw_circle_filled(x - 20, y + 15, 15, arcade.csscolor.GAINSBORO)
-    arcade.draw_circle_filled(x, y + 19, 15, arcade.csscolor.GAINSBORO)
-    arcade.draw_circle_filled(x + 20, y + 15, 15, arcade.csscolor.GAINSBORO)
-    arcade.draw_circle_filled(x + 40, y + 5, 15, arcade.csscolor.GAINSBORO)
-    arcade.draw_circle_filled(x + 25, y - 5, 15, arcade.csscolor.GAINSBORO)
-    arcade.draw_circle_filled(x - 25, y - 5, 15, arcade.csscolor.GAINSBORO)
+    arcade.draw_ellipse_filled(x, y, 80, 40, arcade.csscolor.GAINSBORO, num_segments=32)
+    arcade.draw_circle_filled(x - 40, y + 5, 15, arcade.csscolor.GAINSBORO, num_segments=32)
+    arcade.draw_circle_filled(x - 20, y + 15, 15, arcade.csscolor.GAINSBORO, num_segments=32)
+    arcade.draw_circle_filled(x, y + 19, 15, arcade.csscolor.GAINSBORO, num_segments=32)
+    arcade.draw_circle_filled(x + 20, y + 15, 15, arcade.csscolor.GAINSBORO, num_segments=32)
+    arcade.draw_circle_filled(x + 40, y + 5, 15, arcade.csscolor.GAINSBORO, num_segments=32)
+    arcade.draw_circle_filled(x + 25, y - 5, 15, arcade.csscolor.GAINSBORO, num_segments=32)
+    arcade.draw_circle_filled(x - 25, y - 5, 15, arcade.csscolor.GAINSBORO, num_segments=32)
 
+def main():
+    # Moon
+    arcade.draw_circle_filled(400, 300, 200, arcade.csscolor.BEIGE)
 
-# Moon
-arcade.draw_circle_filled(400, 300, 200, arcade.csscolor.BEIGE)
+    draw_cloud(50, 400)
+    draw_cloud(300, 525)
+    draw_cloud(175, 200)
+    draw_cloud(650, 550)
+    draw_cloud(775, 275)
 
-draw_cloud(50,400)
-draw_cloud(300, 525)
-draw_cloud(175, 200)
-draw_cloud(650, 550)
-draw_cloud(775, 275)
+    draw_star(100, 400)
+    draw_star(500, 550)
+    draw_star(150, 500)
+    draw_star(75, 580)
+    draw_star(50, 200)
+    draw_star(250, 565)
+    draw_star(550, 500)
+    draw_star(675, 450)
+    draw_star(700, 230)
+    draw_star(780, 300)
+    draw_star(745, 555)
 
-draw_star(100, 400)
-draw_star(500, 550)
-draw_star(150, 500)
-draw_star(75, 580)
-draw_star(50, 200)
-draw_star(250, 565)
-draw_star(550, 500)
-draw_star(675, 450)
-draw_star(700, 230)
-draw_star(780, 300)
-draw_star(745, 555)
+    draw_pumpkin(100, 100)
+    draw_pumpkin(50, 50)
+    draw_pumpkin(725, 75)
 
-draw_pumpkin(100, 100)
-draw_pumpkin(50, 50)
-draw_pumpkin(725, 75)
+    draw_witch_hat(50, 75)
+    draw_witch_hat(275, 255)
+    draw_witch_hat(750, 50)
 
-draw_witch_hat(50, 75)
-draw_witch_hat(275, 255)
-draw_witch_hat(750, 50)
+    # --- Draw the castle ---
+    # Castle base
+    arcade.draw_rectangle_filled(400, 150, 500, 200, arcade.csscolor.DIM_GREY)
 
-# --- Draw the castle ---
-# Castle base
-arcade.draw_rectangle_filled(400, 150, 500, 200, arcade.csscolor.DIM_GREY)
+    # Castle Door
+    arcade.draw_arc_filled(400, 50, 200, 300, arcade.csscolor.SADDLE_BROWN, 0, 180)
 
-# Castle Door
-arcade.draw_arc_filled(400, 50, 200, 300, arcade.csscolor.SADDLE_BROWN, 0, 180)
+    # Middle tower
+    arcade.draw_rectangle_filled(400, 300, 200, 100, arcade.csscolor.DIM_GREY)
 
-# Middle tower
-arcade.draw_rectangle_filled(400, 300, 200, 100, arcade.csscolor.DIM_GREY)
+    # Windows on Middle tower
+    draw_window(325, 275)
+    draw_window(475, 275)
 
-# Windows on Middle tower
-draw_window(325, 275)
-draw_window(475, 275)
+    # Door on Middle tower
+    arcade.draw_rectangle_filled(400, 275, 50, 50, arcade.csscolor.SADDLE_BROWN)
 
-# Door on Middle tower
-arcade.draw_rectangle_filled(400, 275, 50, 50, arcade.csscolor.SADDLE_BROWN)
+    # Door handles
+    arcade.draw_circle_filled(395, 275, 3, arcade.csscolor.BLACK)
+    arcade.draw_circle_filled(405, 275, 3, arcade.csscolor.BLACK)
 
-# Door handles
-arcade.draw_circle_filled(395, 275, 3, arcade.csscolor.BLACK)
-arcade.draw_circle_filled(405, 275, 3, arcade.csscolor.BLACK)
+    # Top of Middle tower
+    arcade.draw_triangle_filled(275, 325, 525, 325, 400, 550, arcade.csscolor.DARK_GREY)
 
-# Top of Middle tower
-arcade.draw_triangle_filled(275, 325, 525, 325, 400, 550, arcade.csscolor.DARK_GREY)
+    # Flag on middle tower
+    arcade.draw_triangle_filled(400, 600, 400, 570, 440, 585, arcade.csscolor.DARK_RED)
 
-# Flag on middle tower
-arcade.draw_triangle_filled(400, 600, 400, 570, 440, 585, arcade.csscolor.DARK_RED)
+    # Flag pole on middle tower
+    arcade.draw_line(400, 550, 400, 600, arcade.csscolor.BLACK, 2)
 
-# Flag pole on middle tower
-arcade.draw_line(400, 550, 400, 600, arcade.csscolor.BLACK, 2)
+    # Left Side tower
+    # tower
+    arcade.draw_rectangle_filled(200, 275, 100, 50, arcade.csscolor.DIM_GREY)
 
-# Left Side tower
-# tower
-arcade.draw_rectangle_filled(200, 275, 100, 50, arcade.csscolor.DIM_GREY)
+    draw_window(200, 275)
 
-draw_window(200, 275)
+    # Top of tower
+    arcade.draw_triangle_filled(125, 300, 275, 300, 200, 425, arcade.csscolor.DARK_GREY)
 
-# Top of tower
-arcade.draw_triangle_filled(125, 300, 275, 300, 200, 425, arcade.csscolor.DARK_GREY)
+    # Flag on tower
+    arcade.draw_triangle_filled(200, 475, 200, 445, 240, 460, arcade.csscolor.DARK_RED)
 
-# Flag on tower
-arcade.draw_triangle_filled(200, 475, 200, 445, 240, 460, arcade.csscolor.DARK_RED)
+    # Flag pole on left tower
+    arcade.draw_line(200, 425, 200, 475, arcade.csscolor.BLACK, 2)
 
-# Flag pole on left tower
-arcade.draw_line(200, 425, 200, 475, arcade.csscolor.BLACK, 2)
+    # Right side tower
+    # tower
+    arcade.draw_rectangle_filled(600, 275, 100, 50, arcade.csscolor.DIM_GREY)
 
+    # window
+    draw_window(600, 275)
 
-# Right side tower
-# tower
-arcade.draw_rectangle_filled(600, 275, 100, 50, arcade.csscolor.DIM_GREY)
+    # Top of Right Tower
+    arcade.draw_triangle_filled(525, 300, 675, 300, 600, 425, arcade.csscolor.DARK_GREY)
 
-# window
-draw_window(600, 275)
+    # Flag on Right tower
+    arcade.draw_triangle_filled(600, 475, 600, 445, 640, 460, arcade.csscolor.DARK_RED)
 
-# Top of Right Tower
-arcade.draw_triangle_filled(525, 300, 675, 300, 600, 425, arcade.csscolor.DARK_GREY)
+    # Flag pole on right tower
+    arcade.draw_line(600, 425, 600, 475, arcade.csscolor.BLACK, 2)
 
-# Flag on Right tower
-arcade.draw_triangle_filled(600, 475, 600, 445, 640, 460, arcade.csscolor.DARK_RED)
+    # Finish drawing
+    arcade.finish_render()
 
-# Flag pole on right tower
-arcade.draw_line(600, 425, 600, 475, arcade.csscolor.BLACK, 2)
+    # Keep the window up until someone closes it
+    arcade.run()
 
-
-# Finish drawing
-arcade.finish_render()
-
-# Keep the window up until someone closes it
-arcade.run()
+# Call the main function to get the program started
+main()
---
+++ Testing/9/ch. 10.py
@@ -0,0 +1,14 @@
+# Write a function that takes three numbers
+# and returns the average
+
+def average_three(x, y, z):
+    result = (x + y + z) / 3
+    return result
+
+# Print result of 10, 20, 30
+my_result = average_three(10, 20, 30)
+print(my_result)
+
+
+# "If" Statement
+# "conditional logic"
